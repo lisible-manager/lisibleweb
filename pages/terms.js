15f4581a50508ad032b75070899f6a97f7bb32b5
lisible-manager <cmo.lablitteraire7@gmail.com>
2025-09-18 16:49:50 -0400

    terms.js

--- pages/terms.js
+++ pages/terms.js
@@ -1,3 +1,10 @@
+import Layout from "../components/Layout";
+
 export default function Terms() {
-  return <h1>Termes et Conditions</h1>;
+  return (
+    <Layout>
+      <h1>Politique de confidentialité & CGU</h1>
+      <p>Bienvenue sur Lisible. En utilisant notre plateforme, vous acceptez nos termes...</p>
+    </Layout>
+  );
 }
