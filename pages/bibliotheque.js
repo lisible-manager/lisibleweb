50d1445ebb04382d4f5a9d670402f5e7663dc3d4
lisible-manager <cmo.lablitteraire7@gmail.com>
2025-09-18 16:46:41 -0400

    bibliotheque.js

--- pages/bibliotheque.js
+++ pages/bibliotheque.js
@@ -1,3 +1,18 @@
+import Layout from "../components/Layout";
+import PostCard from "../components/PostCard";
+
 export default function Bibliotheque() {
-  return <h1>Bibliothèque</h1>;
+  const books = [
+    { id: "3", title: "Les Mots et les Songes", excerpt: "Anthologie littéraire", author: "Jean-Paul" },
+    { id: "4", title: "Lettres de Feu", excerpt: "Recueil engagé", author: "Rosa" },
+  ];
+
+  return (
+    <Layout>
+      <h1>Bibliothèque</h1>
+      {books.map((b) => (
+        <PostCard key={b.id} post={b} />
+      ))}
+    </Layout>
+  );
 }
