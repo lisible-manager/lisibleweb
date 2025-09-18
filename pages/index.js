76c826fec881be06af60aa8033cd3f8441eae998
lisible-manager <cmo.lablitteraire7@gmail.com>
2025-09-18 16:45:47 -0400

    index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,18 @@
+import Layout from "../components/Layout";
+import PostCard from "../components/PostCard";
+
 export default function Home() {
-  return <h1>Bienvenue sur Lisible 📚</h1>;
+  const posts = [
+    { id: "1", title: "La poésie du futur", excerpt: "Un voyage dans l'âme...", author: "Ben Johnson Juste" },
+    { id: "2", title: "Le roman du siècle", excerpt: "Découvrez un chef-d'œuvre moderne...", author: "Marie Paul" },
+  ];
+
+  return (
+    <Layout>
+      <h1 style={{ textAlign: "center", marginBottom: "20px" }}>Bienvenue sur Lisible 📚</h1>
+      {posts.map((p) => (
+        <PostCard key={p.id} post={p} />
+      ))}
+    </Layout>
+  );
 }
